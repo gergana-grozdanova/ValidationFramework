1a3db627ac0dbfc9b2008286764365ebb0d79d11
gergana_grozdanova <geri.grozdanova04@gmail.com>
2022-11-28 15:28:56 +0200

    Add project files.

---
+++ ValidationFramework/ValidFoodName.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ValidationFramework
+{
+   sealed public class ValidFoodName:ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+          
+            string type=  value.GetType().ToString();
+            if (type!= "System.String")
+            {
+                return false;
+            }
+            if (value.ToString().Contains("."))
+            {
+                return  false;
+            }
+            return true;
+        }
+    }
+}
---
+++ ValidationFramework/ValidNameLength.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ValidationFramework
+{
+    [System.AttributeUsage(System.AttributeTargets.Property)]
+                   
+    sealed public class ValidNameLength:ValidationAttribute
+    {
+        public ValidNameLength()
+        {
+
+        }
+
+        public override bool IsValid(object value)
+        {
+            bool result = true;
+            if (value.ToString().Length<3)
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+}
